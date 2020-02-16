423b69852db13b6d26985c0051478cdfd5ce8fad
Neema <neemashiramba@gmail.com>
2020-02-17 00:18:52 +0300

    add links

---
+++ README.md
@@ -0,0 +1,27 @@
+# Akan
+
+#### {This app helps users to find out what their Akan name is using the date of their birth.}, 
+{currents version was released on 16/02/2020}
+
+#### By **{Neema Shiramba}**
+
+## Discription
+{An Akan name is determined by the day in which an individual was born, this app therefore calculates
+the specific date when an individual was born and assigns them the correct Akan name. }
+
+## Setup/Installation Requirements
+* Fill in your date of birth in the space provided
+* Submit for calculation
+
+## Known Bugs
+{There are no known Bugs.}
+
+### Technologies Used
+{I used HTML, Bootstrap and Javascript}
+
+## Contact me on: neemashiramba@gmail.com
+{I encourage anyone who has any contribution to make to this code to improve it do so. 
+Live link:https://neema-bmb.github.io/Akan/}
+
+### License
+*{MIT.See below for more details on licensing}*
--- index.js
+++ index.js
@@ -1,39 +1,43 @@
  
- function akanAge(){
-   var CC=parseInt(year.slice(0,2) 
-   var year=Document.getElementById("Year").value;
-   var YY=parseInt(year.slice (2,4))
-   var MM=parseInt.Documents.getElementId("Month").value;
+ function akanNames(){
+  
+   var CC=parseInt(Year.slice(0,2))
+   var Year=Document.getElementById("Year").value;
+   var YY=parseInt(Year.slice (2,4))
+   var MM=parseInt.Document.getElementId("Month").value;
    var DD=parseInt.Document.getElementId("Day").value;
    
-   var weekday=day (CC, YY, MM, DD, mod)
-   var day=weekdays.fixed(0)
-  }
-
-   var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-   var maleName=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw","Kofi","Kwame"]
-   var femaleName=["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+   
+   var male=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw","Kofi","Kwame"]
+   var female=["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
    var gender=document.getElementById("gender").value;
-   console.log(gender)
+   console.log(gender);
 
-   var CalculateDate=( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
-   CalculateDate= Math.floor(CalculateDate);
+   var CalculateDate = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7;
+   CalculateDate= Math.floor(CalculateDate)
 
    if(MM<1||MM>12){
-     alert("Invalid month")
-   };
+     alert("Invalid Month");
+   }
    if(DD>31||DD<1){
-     alert("Invalid Date")
-   };
-   if(gender==="male"){
-     sex =male;
-   };
-   else {
-     sex=female;
-   };
-
-   var output=sex[CalculateDate]
-   document.getElementById("answer").innerHtml="Your Akan name is"+output+".";
-   };
+     alert("Invalid Date");
+   }
    
- 
+   
+   if(gender==="male"){
+     sex = male;
+   }
+   else{
+     sex = female;
+   }
+
+   var output = sex[CalculateDate]
+   document.getElementById("answer").innerHtml = "Your Akan name is"+output+".";
+  }
+   
+   
+  var weekdays =day (CC, YY, MM, DD)
+   var day=weekdays.fixed(0)
+  
+
+   var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
